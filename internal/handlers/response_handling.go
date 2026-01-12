88b36faf9e3fbc6407d35dc863607dc8ac397e6b
Hithesh Krishna Moolya <moolya@innoscripta.com>
2026-01-13 00:32:35 +0530

    chore: remove unnessasry code

--- internal/handlers/response_handling.go
+++ internal/handlers/response_handling.go
@@ -1,11 +1,7 @@
 package handlers
 
 import (
-	"fmt"
-	"mime"
 	"net/http"
-	"path/filepath"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -41,13 +37,6 @@ func UnprocessableEntityResponse(c *gin.Context, message string) {
 		Success:    false,
 		Error:      message,
 		StatusCode: http.StatusUnprocessableEntity,
-	})
-}
-
-func NoContentResponse(c *gin.Context) {
-	c.JSON(http.StatusNoContent, Response{
-		Success:    false,
-		StatusCode: http.StatusNoContent,
 	})
 }
 
@@ -109,68 +98,4 @@ func UnauthorizedResponse(c *gin.Context, err error) {
 		Error:      err.Error(),
 		StatusCode: http.StatusUnauthorized,
 	})
-}
-
-func BytesResponse(c *gin.Context, filename string, contentType string, data []byte) {
-	// Auto-detect content type if not provided
-	if contentType == "" {
-		// 1. Try by file extension
-		contentType = mime.TypeByExtension(filepath.Ext(filename))
-
-		// 2. Fallback: detect from data
-		if contentType == "" {
-			contentType = http.DetectContentType(data)
-		}
-	}
-
-	contentDisposition := c.DefaultQuery("response-content-disposition", "attachment")
-
-	if contentDisposition != "inline" && contentDisposition != "attachment" {
-		contentDisposition = "attachment"
-	}
-
-	c.Header("Content-Disposition", fmt.Sprintf("%s; filename=%s", contentDisposition, filename))
-	c.Header("Content-Type", contentType)
-	c.Header("Content-Length", fmt.Sprintf("%d", len(data)))
-
-	c.Writer.WriteHeader(http.StatusOK)
-	_, _ = c.Writer.Write(data)
-	c.Abort()
-}
-
-func SuccessXMLResponse(c *gin.Context, data interface{}) {
-	// Get Accept header from request
-	acceptHeader := c.GetHeader("Accept")
-
-	// Default to XML, return JSON only if explicitly requested
-	switch {
-	case strings.Contains(strings.ToLower(acceptHeader), "application/json"):
-		c.JSON(http.StatusOK, Response{
-			Success:    true,
-			Data:       data,
-			StatusCode: http.StatusOK,
-		})
-	default:
-		// Default to XML for all other cases (including no header, */*, application/xml, etc.)
-		c.XML(http.StatusOK, Response{
-			Success:    true,
-			Data:       data,
-			StatusCode: http.StatusOK,
-		})
-	}
-}
-
-func ErrorXMLResponse(c *gin.Context, statusCode int, errorMessage interface{}) {
-	// Get Accept header from the request
-	acceptHeader := c.GetHeader("Accept")
-
-	// Check Accept header and respond accordingly (JSON or XML)
-	switch {
-	case strings.Contains(strings.ToLower(acceptHeader), "application/json"):
-		// If the client expects JSON, return the error response in JSON format
-		c.JSON(statusCode, errorMessage)
-	default:
-		// Default to XML (for application/xml or no header specified)
-		c.XML(statusCode, errorMessage)
-	}
 }
